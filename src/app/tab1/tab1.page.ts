9edae97e228ee0b53a31f4755b6de59a4f4f1812
Dominic DeJesus <domludejesus@gmail.com>
2022-07-14 19:23:50 -0400

    "creating components & adding click functions"

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  title = "Grocery List"; 
 
+  items = [
+    {
+      name: "Milk", 
+      quantity: 2
+    },
+    {
+      name: "Bread", 
+      quantity: 5
+    },
+    {
+      name: "Banana", 
+      quantity: 6
+    },
+    {
+      name: "Sugar", 
+      quantity: 10
+    },
+  ];
   constructor() {}
 
+  removeItem(item) {
+    console.log("Removing Item -", item)
+  }
+
 }
+
+
+
